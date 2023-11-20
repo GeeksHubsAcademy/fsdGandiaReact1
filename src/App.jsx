afaa86ac9c69f1d6e66ae5264a66be0c1b1d3ff7
Dave86dev <davidochando1986@gmail.com>
2023-11-20 10:36:43 +0100

    Estructura inicial básica e inicio de proyecto en React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import { Header } from './common/Header/Header'
+import { Home } from './pages/Home/Home'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+     <Home />
+    </div>
+  )
+}
+
+export default App
