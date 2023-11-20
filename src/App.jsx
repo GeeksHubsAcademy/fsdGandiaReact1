3019c92bdf230d9ba3879516cebd2dcd8c6fa382
Dave86dev <davidochando1986@gmail.com>
2023-11-20 10:49:48 +0100

    Sección Login avanzada al mínimo

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 
 import { Header } from './common/Header/Header'
 import { Home } from './pages/Home/Home'
+import { Login } from './pages/Login/Login'
 
 function App() {
 
   return (
     <div>
       <Header />
-     <Home />
+      <Home />
+      {/* <Login /> */}
     </div>
   )
 }
